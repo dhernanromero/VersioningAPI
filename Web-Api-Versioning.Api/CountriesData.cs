058a4dc0b9a3685349436d88f78c4c065d55dd9d
dhernanromero <diego.romero@externos-ar.cencosud.com>
2023-09-25 02:11:56 -0300

    initial files

---
+++ Web-Api-Versioning.Api/CountriesData.cs
@@ -0,0 +1,21 @@
+﻿using Web_Api_Versioning.Api.Models.Domain;
+
+namespace Web_Api_Versioning.Api
+{
+    public static class CountriesData
+    {
+        public static List<Country> Get()
+        {
+            var countries = new[]
+            {
+                new {Id = 1 , Name = "Argentina" },
+                new {Id = 2 , Name = "United States" },
+                new {Id = 3 , Name = "Japan" },
+                new {Id = 4 , Name = "Mexico" },
+                new {Id = 5 , Name = "China" }
+            };
+
+            return countries.Select( c => new Country { Id = c.Id, Name = c.Name }).ToList();
+        }
+    }
+}
---
+++ Web-Api-Versioning.Api/Models/DTOs/CountryDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Web_Api_Versioning.Api.Models.DTOs
+{
+    public class CountryDtoV1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    public class CountryDtoV2
+    {
+        public int Id { get; set; }
+        public string CountryName { get; set; }
+
+    }
+}
